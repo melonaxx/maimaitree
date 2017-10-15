b36287231a37ec9880980e7787baa8ab3ff8f3fd
tianyaqiang <tianyaqiang@guixue.com>
2017-10-15 21:44:26 +0800

    repeat

--- app/Http/Controllers/Api/YanLingAPIController.php
+++ app/Http/Controllers/Api/YanLingAPIController.php
@@ -23,7 +23,7 @@ class YanLingAPIController extends AppBaseController
         $license = $request->input('license','');
         $search_data = array('types'=>$types,'license'=>$license);
         $v_data = Utils::simpleRequest($this->trafficViolationUrl, $search_data);
-
+        header('Access-Control-Allow-Origin:*');
         $break_pos = strrpos($v_data,'<div class="well">');
         if ($break_pos) {
             return substr($v_data,0,$break_pos);
